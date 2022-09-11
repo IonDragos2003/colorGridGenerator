e831f3058d64655d6a7908566f66dab3d46f862d
IonDragos2003 <iondragos2003@gmail.com>
2022-09-11 09:54:21 +0300

    "PyQt5 working but Global"

--- vlc_player_final.py
+++ vlc_player_final.py
@@ -1,32 +1,9 @@
-#! /usr/bin/python
-
-#
-# Qt example for VLC Python bindings
-# Copyright (C) 2009-2010 the VideoLAN team
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
-#
-
 import sys
 import os.path
-from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QPalette, QColor
-from PyQt6.QtWidgets import QMainWindow, QWidget, QFrame, QSlider, QHBoxLayout, QPushButton, \
-    QVBoxLayout, QFileDialog, QApplication
-from PyQt6.QtGui import QAction
-
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import QMainWindow, QWidget, QFrame, QSlider, QHBoxLayout, QPushButton, \
+    QVBoxLayout, QAction, QFileDialog, QApplication
 import vlc
 
 class Player(QMainWindow):
@@ -52,10 +29,8 @@ class Player(QMainWindow):
 
         # In this widget, the video will be drawn
         if sys.platform == "darwin": # for MacOS
-            #from PyQt6.QtWidgets import QMacCocoaViewContainer
-            #from PyQt6.QtWidgets import QWIND
-            #self.videoframe = QMacCocoaViewContainer(0)
-            self.videoframe = QWidget.createWindowContaineR
+            from PyQt5.QtWidgets import QMacCocoaViewContainer	
+            self.videoframe = QMacCocoaViewContainer(0)
         else:
             self.videoframe = QFrame()
         self.palette = self.videoframe.palette()
